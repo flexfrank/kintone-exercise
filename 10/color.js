23d58622b372dae08d33f79fa3335a12d4e415a4
cy2014b <yohei-yamaguchi@cybozu.co.jp>
2014-04-21 16:49:03 +0900

    課題10-3訂正

--- 10/color.js
+++ 10/color.js
@@ -10,7 +10,7 @@
     // レコード一覧の表示時にフィールドの背景色を変更する
     kintone.events.on('app.record.index.show', function (event) {
             // ログインユーザのフィールド色
-        var fieldColor = ['#87cefa', '#adff2f', '#ffd700', '#ff6347', '#d3d3d3', '#4b0082'],
+        var fieldColorList = ['#87cefa', '#adff2f', '#ffd700', '#ff6347', '#d3d3d3', '#4b0082'],
             // 一覧の要素を取得
             elCustomer = kintone.app.getFieldElements('Customer'),
             elStatus = kintone.app.getFieldElements('Status'),
@@ -23,12 +23,12 @@
         for (i = 0; i < event.records.length; i++) {
             if (i % 2 !== 0) {
                 j = Math.floor(Math.random() * 6);
-                elCustomer[i].style.backgroundColor = fieldColor[j];
-                elStatus[i].style.backgroundColor = fieldColor[j];
-                elPerson[i].style.backgroundColor = fieldColor[j];
-                elQType[i].style.backgroundColor = fieldColor[j];
-                elDetail[i].style.backgroundColor = fieldColor[j];
-                elLimitDay[i].style.backgroundColor = fieldColor[j];
+                elCustomer[i].style.backgroundColor = fieldColorList[j];
+                elStatus[i].style.backgroundColor = fieldColorList[j];
+                elPerson[i].style.backgroundColor = fieldColorList[j];
+                elQType[i].style.backgroundColor = fieldColorList[j];
+                elDetail[i].style.backgroundColor = fieldColorList[j];
+                elLimitDay[i].style.backgroundColor = fieldColorList[j];
             }
         }
     });
