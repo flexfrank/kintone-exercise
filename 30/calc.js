0bfc873c8b195e71cf7a5b0126f5d90ad6b392d1
cy2014b <yohei-yamaguchi@cybozu.co.jp>
2014-04-22 10:50:58 +0900

    課題30-3

--- 30/calc.js
+++ 30/calc.js
@@ -14,7 +14,9 @@
         //既に入っている要素を削除
         headerSpace.innerHTML = '';
         
-        sum = parseInt(event.records[0]['数値_1'].value, 10);
+        for (var i = 0; i < event.records.length; i++) {
+            sum += parseFloat(event.records[i]['数値_1'].value, 10);
+        }
         textNode = document.createTextNode('合計金額: ' + String(sum));
         headerSpace.style.fontSize = '24pt';
         headerSpace.style.color = '#006400';
