57aad07aa8ef86fa46766dfadca1dd6f8ce04b5c
Cyhisa <taiga>
2014-04-30 14:45:46 +0900

    余計なコメント削除

--- 10/color.js
+++ 10/color.js
@@ -9,16 +9,9 @@
     "use strict";
     // レコード一覧の表示時にフィールドの背景色を変更する
     kintone.events.on('app.record.index.show', function (event) {
-            // ログインユーザのフィールド色
+        // ログインユーザのフィールド色
         var fieldColor = '#e5f0ff';
-            // 一覧の要素を取得
-            /*elCustomer = kintone.app.getFieldElements('Customer'),
-            elStatus = kintone.app.getFieldElements('Status'),
-            elPerson = kintone.app.getFieldElements('Person'),
-            elQType = kintone.app.getFieldElements('QType'),
-            elDetail = kintone.app.getFieldElements('Detail'),
-            elLimitDay = kintone.app.getFieldElements('LimitDay'),
-            i;*/
+        // 一覧の要素を取得
         var elCustomer = kintone.app.getFieldElements('Customer');
         var elStatus = kintone.app.getFieldElements('Status');
         var elPerson = kintone.app.getFieldElements('Person');
