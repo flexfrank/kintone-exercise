162d375c6678693c9953aac457232364c252ad10
h-maeta <hirokuni.maeta@gmail.com>
2014-05-01 10:37:31 +0900

    課題32　calc.js変更

--- 30/calc.js
+++ 30/calc.js
@@ -13,9 +13,12 @@
 
         //既に入っている要素を削除
         headerSpace.innerHTML = '';
-
-        sum = parseInt(event.records[0]['数値_1'].value, 10);
+        var i = 0;
+        for (i = 0; i < event.records.length; i++) {
+            sum = sum + parseInt(event.records[i]['数値_1'].value, 10);
+        }
         textNode = document.createTextNode('合計金額: ' + sum);
         headerSpace.appendChild(textNode);
+        headerSpace.style.fontSize = '24pt';
     });
 }());
